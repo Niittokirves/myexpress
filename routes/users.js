2a3be05786769996bd6146866a038056fd4e9c66
Niittokirves <laaksolate@gmail.com>
2022-02-20 19:49:13 +0200

    Express alustettu

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,27 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/tervehdys',function(request,response)
+{
+        response.send("Huomenta!");
+        console.log("Good Night!");
+    });
+
+router.get('/tervehdys/:etunimi',function(request,response){
+  response.send('Terve '+request.params.etunimi);
+  console.log('Tulostus onnistui!');
+});
+
+router.get('/tervehdys/:etunimi/:sukunimi',function(request,response){
+  response.send('Tervehdys vaan '+request.params.etunimi+' '+request.params.sukunimi);
+  console.log('Tulostus onnistui taas!');
+});
+
+router.post('/',
+    function(request,response){
+        response.send(request.body);
+        console.log(request.body);
+    }
+);
+
 module.exports = router;
